c9de461971e5ee3ad7457640c01f7e22890acd6c
GageMG <gagekace@gmail.com>
2025-11-12 01:10:28 -0500

    started the GET Requests.

--- backend/EndyPoint.py
+++ backend/EndyPoint.py
@@ -1,2 +1,16 @@
-import firebase_admin;
+import requests
 
+#to define the API URL
+url = "THENWEPASTEITINHERE"
+
+#Going to write the GET 
+try:
+    response = requests.get(url)
+
+    #to check successful response, I guess successful response is code 200
+    if response.get() == 200:
+        data = response.json()
+        print("Data retrieved")
+        print(data)
+    else:
+        
