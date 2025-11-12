53ec4f10bb5b9e4972e1b3193e6b3cb57490e496
GageMG <gagekace@gmail.com>
2025-11-12 16:05:28 -0500

    Updated backend

---
+++ backend/API_Handling/JSO_me.py
@@ -0,0 +1,16 @@
+from flask import Flask, url_for
+
+
+@app.route("/me")
+def me_api():
+    user = get_current_user()
+    return {
+        "username": user.username,
+        "theme": user.theme,
+        "image": url_for("user_image", filename=user.image),
+    }
+
+@app.route("/users")
+def user_api():
+    users = get_all_users()
+    return [user.to_json() for user in users]
