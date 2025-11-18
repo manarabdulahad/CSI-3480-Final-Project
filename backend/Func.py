39eee6e395de6e62f78f88195d3b4134fdffb30a
Gage <gagekace@gmail.com>
2025-11-18 01:21:19 -0500

    started base backend functions

--- backend/Func.py
+++ backend/Func.py
@@ -8,3 +8,10 @@ def db_get_salt(email):
 
 def db_register_user(email, salt, verifier):
   return create_user(email, salt, verifier)
+
+
+def db_delete_user(guid):
+  return delete_user(guid)
+
+def db_create_item(user_guid, name, username, password):
+  return create_item(user_guid, name, username, password)
